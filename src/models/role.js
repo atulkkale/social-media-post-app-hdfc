62a3ef6dc3f33b5b5113a0c36c75b8325d33b638
Atul Kale <atulk@axioned.com>
2023-04-02 19:44:19 +0530

    Added timestamps.

--- src/models/role.js
+++ src/models/role.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const roleSchema = mongoose.Schema({
-  role_name: {
-    type: String,
-    required: true,
+const roleSchema = mongoose.Schema(
+  {
+    role_name: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Role', roleSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role_id: {
+      type: Number,
+      enum: [1, 2, 3],
+      default: 2,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role_id: {
-    type: Number,
-    enum: [1, 2, 3],
-    default: 2,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 userSchema.pre('save', async function (next) {
   if (this.password.length < 60) {
