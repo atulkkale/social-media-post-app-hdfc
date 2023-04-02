819a77e0f461259c9716d09d80a8766387b0e52b
Atul Kale <atulk@axioned.com>
2023-04-02 21:36:15 +0530

    Added updatedPost controller.

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -3,30 +3,31 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 const Post = require('../models/post');
 
+const postMessageSchema = Joi.object().keys({
+  post_message: Joi.string().required(),
+});
+const postMessageSchemaOpt = { abortEarly: false };
+
 exports.createPost = async (req, res) => {
   try {
     // Validation
     const { user_id } = req.params;
+    const { id } = req.user;
     const isValidId = mongoose.Types.ObjectId.isValid(user_id);
     if (!isValidId)
       return res.status(403).json(utils.responseMsg('Invalid Request Id!'));
-    const createPostSchema = Joi.object().keys({
-      post_message: Joi.string().required(),
-    });
-    const createPostSchemaOpt = { abortEarly: false };
     const validationResult = utils.validateData(
-      createPostSchema,
+      postMessageSchema,
       req.body,
-      createPostSchemaOpt
+      postMessageSchemaOpt
     );
-    if (validationResult) {
+    if (validationResult)
       return res.status(403).json(utils.responseMsg(validationResult));
-    }
+
+    if (user_id !== id)
+      return res.status(403).json(utils.responseMsg('Make post for yourself!'));
     // Collect previous messages if any
-    const previousMessages = await Post.find(
-      { user_id: user_id },
-      { _id: 1 }
-    ).sort({
+    const previousMessages = await Post.find({ user_id: id }, { _id: 1 }).sort({
       createdAt: -1,
     });
     // Create post
@@ -36,7 +37,7 @@ exports.createPost = async (req, res) => {
       previous_message: previousMessages,
       is_active: 0,
     });
-    return res.json({ message: 'Post created successfully' });
+    return res.status(200).json({ message: 'Post created successfully' });
   } catch (error) {
     console.log(error);
     return res.status(500).json(utils.responseMsg(error.message));
@@ -44,7 +45,34 @@ exports.createPost = async (req, res) => {
 };
 
 exports.updatePost = async (req, res) => {
-  res.json({ message: 'Post updated successfully' });
+  try {
+    // Validation
+    const { post_id } = req.params;
+    const isValidId = mongoose.Types.ObjectId.isValid(post_id);
+    if (!isValidId)
+      return res.status(403).json(utils.responseMsg('Invalid Request Id!'));
+    const validationResult = utils.validateData(
+      postMessageSchema,
+      req.body,
+      postMessageSchemaOpt
+    );
+    if (validationResult) {
+      return res.status(403).json(utils.responseMsg(validationResult));
+    }
+    // Update post
+    const post = await Post.findOne({
+      _id: post_id,
+      user_id: req.user._id,
+    });
+    if (!post)
+      return res.status(404).json(utils.responseMsg('Post not found!'));
+    post.post_message = req.body.post_message;
+    await post.save();
+    return res.status(200).json(utils.responseMsg('Post updated successfully'));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(utils.responseMsg(error.message));
+  }
 };
 
 exports.deletePost = async (req, res) => {
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,8 +1,7 @@
 const passport = require('passport');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const User = require('../models/user');
-const { secret, expireIn, algorithm } = require('../config/config').jwt;
-const jwt = require('jsonwebtoken');
+const { secret, algorithm } = require('../config/config').jwt;
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -22,23 +21,3 @@ passport.use(
     }
   })
 );
-
-/**
- * @name createToken
- * @param {Object} data - Object containing data which needs to be stored in token
- * @description Returns signed JWT token which can be given to client
- */
-exports.createToken = (data) => {
-  try {
-    if (data === Object(data)) {
-      return jwt.sign(data, secret, {
-        algorithm: algorithm,
-        expiresIn: expireIn,
-      });
-    } else {
-      return new Error('Given data is not object.');
-    }
-  } catch (error) {
-    return error;
-  }
-};
