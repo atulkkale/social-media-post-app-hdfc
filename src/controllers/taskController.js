b994f01cb401a67a24ce1e387de1a0ea141d8122
Atul Kale <atulk@axioned.com>
2023-04-02 11:41:48 +0530

    Added createPost controller.

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,5 +1,35 @@
+const utils = require('../helpers/utils');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const Post = require('../models/post');
+
 exports.createPost = async (req, res) => {
-  res.json({ message: 'Post created successfully' });
+  try {
+    // Validation
+    const { user_id: id } = req.params;
+    const isValidId = mongoose.Types.ObjectId.isValid(id);
+    if (!isValidId)
+      return res.status(403).send(utils.responseMsg('Invalid Request Id!'));
+    const createPostSchema = Joi.object().keys({
+      post_message: Joi.string().required(),
+    });
+    const createPostSchemaOpt = { abortEarly: false };
+    const validationResult = utils.validateData(
+      createPostSchema,
+      req.body,
+      createPostSchemaOpt
+    );
+    if (validationResult) {
+      return res.status(403).json(utils.responseMsg(validationResult));
+    }
+    // Create post
+
+    // const post = await Post.create({user_id: id, ...req.body, })
+    res.json({ message: 'Post created successfully' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(utils.responseMsg(error.message));
+  }
 };
 
 exports.updatePost = async (req, res) => {
