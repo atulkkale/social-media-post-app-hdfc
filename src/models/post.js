f99e89287796d1b1041abd49b70a56cba26dcfa4
Atul Kale <atulk@axioned.com>
2023-04-02 19:06:56 +0530

    Updated post module.

--- src/models/post.js
+++ src/models/post.js
@@ -1,25 +1,29 @@
 const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-  post_message: {
-    type: String,
-    required: true,
-  },
-  previous_message: [
-    {
-      message: String,
+const postSchema = mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-  ],
-  is_active: {
-    type: Boolean,
-    required: true,
-    default: false,
+    post_message: {
+      type: String,
+      required: true,
+    },
+    previous_message: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'postSchema',
+      },
+    ],
+    is_active: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Post', postSchema);
