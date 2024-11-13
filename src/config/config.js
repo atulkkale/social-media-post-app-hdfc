2bd1d62a57e81ce2c5e3d88e9556ed05809387dc
Atul Kale <kaleatul9@gmail.com>
2024-11-13 14:30:22 +0530

    Updated JWT secret.

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {};
 
 config.jwt = {
-  secret: process.env.JWT_SECRET_KEY || 'THISISASECRET',
+  secret: process.env.JWT_SECRET_KEY,
   expireIn: process.env.JWT_EXPIRE_IN || '300',
   algorithm: process.env.JWT_ALGORITHM || 'HS256',
 };
